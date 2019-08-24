ca4a82dd2f7e1e0209751dc6bbf8cb54a99ac42c
Sage Elliott <hello@sageelliott.com>
2019-08-23 21:24:43 -0700

    Feature: loop thru images in path using waitkye to move forward

--- labeler.py
+++ labeler.py
@@ -1,8 +1,13 @@
 # Import libraries
 import cv2
+import glob
+import os
 
 
 # Point to Dataset
+# in future use argparse
+
+unlabeled_data_path = 'data/'
 
 
 # Define classes for labeling
@@ -11,7 +16,23 @@ import cv2
 # create folder for classes
 
 
+
+
+
+
 # Display first image from dataset
 
 # Assign to new dataset with key
 # move image to designated class folder
+for imagePath in glob.glob(f'{unlabeled_data_path}*.jpg'):
+
+    image = cv2.imread(imagePath)
+    print("width: %d pixels" % (image.shape[1]))
+    print("height: %d pixels" % (image.shape[0]))
+    print("channels: %d" % (image.shape[2]))
+
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+
+
+    # cv2.imwrite("/{class}/newimage.jpg", image)
