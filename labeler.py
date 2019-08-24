0e3c33f5a4344e3498091626bc90c42f5fa557f1
Sage Elliott <hello@sageelliott.com>
2019-08-23 21:02:07 -0700

    Plan out functionality

---
+++ labeler.py
@@ -0,0 +1,17 @@
+# Import libraries
+import cv2
+
+
+# Point to Dataset
+
+
+# Define classes for labeling
+# in future define from Argparse
+# Set key for each class
+# create folder for classes
+
+
+# Display first image from dataset
+
+# Assign to new dataset with key
+# move image to designated class folder
