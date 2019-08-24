e44739ef80af2856ec260d4f1e07ccbe99f04c2b
Sage Elliott <hello@sageelliott.com>
2019-08-23 22:32:28 -0700

    Feat: Move class to variables. labeler is in MVP state

--- labeler.py
+++ labeler.py
@@ -14,42 +14,61 @@ unlabeled_data_path = 'data/'
 # Define classes for labeling
 # in future define from Argparse
 # Set key for each class
+
+
+class1 = "female" 
+class1_key = 'f'
+
+class2 = "male" 
+class2_key = 'm'
+
 # create folder for classes
-
-
-
-# Display first image from dataset
-
-# Assign to new dataset with key
-# move image to designated class folder
+#TODO
+# Currently you must make folders with matching class names
 
 for imagePath in glob.glob(f'{unlabeled_data_path}*.jpg'):
 
     # Get number of files for each class
-    count_class1 = len(os.listdir('female/'))
-    count_class2 = len(os.listdir('male/'))
+    count_class1 = len(os.listdir(f'{class1}/'))
+    count_class2 = len(os.listdir(f'{class2}/'))
     count_nolabel = len(os.listdir('nolabel/'))
     print(count_class1)
     print(count_class2)
 
-    # Read & Display image to be labeled
-    image = cv2.imread(imagePath)
-    cv2.imshow("Image", image)
-    cv2.waitKey(0)
-
     try: 
         if keyboard.is_pressed('f'):
-            cv2.imwrite(f'female/female{count_class1+1}.jpg', image)
-            print('Added to female label')
+            cv2.imwrite(f'{class1}/{class1}{count_class1+1}.jpg', image)
+            print(f'Added to {class1} label')
         
         elif keyboard.is_pressed('m'):
-            cv2.imwrite(f'male/male{count_class2+1}.jpg', image)
-            print('Added to male label')
+            cv2.imwrite(f'{class2}/{class2}{count_class2+1}.jpg', image)
+            print(f'Added to {class2} label')
         
         else:
             cv2.imwrite(f'nolabel/nolabel{count_nolabel+1}.jpg', image)
             print("that key is not defined")
 
     except:
-        break
+        pass
+
+    # Read & Display image to be labeled
+    image = cv2.imread(imagePath)
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+
+    # try: 
+    #     if keyboard.is_pressed('f'):
+    #         cv2.imwrite(f'{class1}/{class1}{count_class1+1}.jpg', image)
+    #         print(f'Added to {class1} label')
+        
+    #     elif keyboard.is_pressed('m'):
+    #         cv2.imwrite(f'{class2}/{class2}{count_class2+1}.jpg', image)
+    #         print(f'Added to {class2} label')
+        
+    #     else:
+    #         cv2.imwrite(f'nolabel/nolabel{count_nolabel+1}.jpg', image)
+    #         print("that key is not defined")
+
+    # except:
+    #     break
 
